53e3f377504e629a45767cc4883d147c39c040a3
Jesus Urtasun <jurtasun@MRC-11016.local>
2022-01-10 10:23:18 +0000

    Prepare course roadmap

---
+++ week1/exercise1.R
@@ -0,0 +1,15 @@
+# Linear Regression in R course
+# Jesus Urtasun - January 2021
+# Week 1 - exercise 1
+
+print("Linear Regression in R course")
+print("Week 1 - exercise 1")
+
+# Correlation - strength of the linear association between two variables
+# Residuals - leftovers from the model fit (diff between observed and predicted y)
+# Least squares line - a measure for the best fit (minimize sum of squares of residuals)
+
+# Conditions for linear regression
+# 1. Linear relationship between x and y
+# 2. Normal residuals
+# 3. Constant variability
