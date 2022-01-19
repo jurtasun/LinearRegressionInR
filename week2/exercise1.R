59f25dfa636b3bb6ec0aa1084a94d95136fe735c
Jesus Urtasun <jurtasun@MRC-11016.local>
2022-01-19 12:00:56 +0000

    Add week1 and week2

---
+++ week2/exercise1.R
@@ -0,0 +1,14 @@
+# Linear Regression in R course
+# Jesus Urtasun - January 2021
+# Week 2 - exercise 1
+
+# Load libraries
+# library("StatsWithR/statsr")
+library("dplyr")
+library("ggplot2")
+
+print("Linear Regression in R course")
+print("Week 2 - exercise 1")
+
+# Inference, hypothesis testing and statistic tests
+# Statistics in R
